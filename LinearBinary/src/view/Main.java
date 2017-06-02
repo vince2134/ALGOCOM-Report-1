55e1b97151bb42f1219ae2275a4914ebbbbdb683
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2017-06-02 14:08:45 +0800

    first commit

---
+++ LinearBinary/src/view/Main.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * Created by avggo on 6/2/2017.
+ */
+public class Main {
+
+    private JComboBox inputSizeComboBox;
+    private JLabel inputSizeLabel;
+    private JLabel inputValueLabel;
+    private JTextField inputValueTextField;
+    private JLabel searchTypeLabel;
+    private JComboBox searchTypeComboBox;
+}
